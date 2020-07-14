0154754b7fde292e28383ada23c4f07b82140214
Prakharsvnit <tprakhar468@gmail.com>
2020-07-14 22:04:49 +0530

    fourth commit

--- app.py
+++ app.py
@@ -22,14 +22,16 @@ class citizen_report(db.Model):
     name = db.Column(db.String(100))
     age = db.Column(db.String(100))
     sex = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
     state = db.Column(db.String(100))
     city = db.Column(db.String(100))
     symptoms = db.Column(db.String(255))
 
-    def __init__(self, name, age, sex, state, city,symptoms):
+    def __init__(self, name, age, sex, phone, state, city,symptoms):
         self.name = name
         self.age = age
         self.sex = sex
+        self.phone = phone
         self.state = state
         self.city = city
         self.symptoms = symptoms
@@ -55,17 +57,28 @@ def insert():
         name = request.form['name']
         age = request.form['age']
         sex = request.form['sex']
+        phone = request.form['phone']
         state = request.form['state']
         city = request.form['city']
         symp = request.form.getlist('symptom')
         symptoms = ','.join(symp)
-        
-        my_report = citizen_report(name,age,sex,state,city,symptoms)
+
+        my_report = citizen_report(name,age,sex,phone,state,city,symptoms)
         db.session.add(my_report)
         db.session.commit()
 
-        return redirect(url_for('login'))
+        return redirect(url_for('report'))
 
 @app.route('/patient')
 def patient():
     return render_template('patient.html')
+
+@app.route('/report',methods=['GET','POST'])
+def report():
+
+    if request.method == 'POST':
+
+        result = citizen_report.query.filter_by(phone = request.form['phone']).first()
+        return render_template('report.html',result=result)
+
+    return render_template('report.html')
