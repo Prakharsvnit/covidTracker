96cbd6eb7497eb112d3dcbefa8f6187b5a257ac3
Prakharsvnit <tprakhar468@gmail.com>
2020-07-16 20:43:40 +0530

    ninth commit

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from flask import (
     request,
     url_for
 )
-
+from fpdf import FPDF
 from flask_sqlalchemy import SQLAlchemy
+import pygal
 
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
@@ -105,8 +106,33 @@ def insertPatient():
 @app.route('/statistics')
 def statistics():
 
+    up_active = len( patient_detail.query.filter_by(state="Uttar Pradesh",status="Active").all())
+    up_recovered = len( patient_detail.query.filter_by(state="Uttar Pradesh",status="Recovered").all())
+    up_deceased = len( patient_detail.query.filter_by(state="Uttar Pradesh",status="Deceased").all())
+    mp_active = len( patient_detail.query.filter_by(state="Madhya Pradesh",status="Active").all())
+    mp_recovered = len( patient_detail.query.filter_by(state="Madhya Pradesh",status="Recovered").all())
+    mp_deceased = len( patient_detail.query.filter_by(state="Madhya Pradesh",status="Deceased").all())
+    maha_active = len( patient_detail.query.filter_by(state="Maharashtra",status="Active").all())
+    maha_recovered = len( patient_detail.query.filter_by(state="Maharashtra",status="Recovered").all())
+    maha_deceased = len( patient_detail.query.filter_by(state="Maharashtra",status="Deceased").all())
+    guj_active = len( patient_detail.query.filter_by(state="Gujarat",status="Active").all())
+    guj_recovered = len( patient_detail.query.filter_by(state="Gujarat",status="Recovered").all())
+    guj_deceased = len( patient_detail.query.filter_by(state="Gujarat",status="Deceased").all())
+    del_active = len( patient_detail.query.filter_by(state="Delhi",status="Active").all())
+    del_recovered = len( patient_detail.query.filter_by(state="Delhi",status="Recovered").all())
+    del_deceased = len( patient_detail.query.filter_by(state="Delhi",status="Deceased").all())
+
+    line_chart = pygal.Bar()
+    line_chart.title = 'Covid-19 cases in different states'
+    line_chart.x_labels = ['Uttar Pradesh','Madhya Pradesh','Maharashtra','Gujarat','Delhi']
+    line_chart.add('Active',[up_active,mp_active,maha_active,guj_active,del_active] )
+    line_chart.add('Recovered',[up_recovered,mp_recovered,maha_recovered,guj_recovered,del_recovered])
+    line_chart.add('Deceased',[up_deceased,mp_deceased,maha_deceased,guj_deceased,del_deceased])
+    chart_data = line_chart.render_data_uri()
+
+
     stats = patient_detail.query.all()
-    return render_template('statistics.html',stats=stats)
+    return render_template('statistics.html',stats=stats,chart_data=chart_data)
 
 @app.route('/patient')
 def patient():
@@ -118,7 +144,36 @@ def report():
     if request.method == 'POST':
 
         result = citizen_report.query.filter_by(phone = request.form['phone']).all()
+        for row in result:
+
+            pdf = FPDF()
+            pdf.add_page()
+            pdf.set_font("Arial","BU",size=30)
+            pdf.cell(190,30,txt ="Your Covid-19 report",ln=1, align="C")
+            pdf.set_font("")
+            pdf.set_font("Arial",size=20)
+            pdf.cell(90,20,txt ="Name :",ln=0,align="R")
+            pdf.cell(100,20,txt = row.name,ln=1,align="L")            
+            pdf.cell(90,20,txt ="Age :",ln=0,align="R")
+            pdf.cell(100,20,txt = row.age,ln=1,align="L")            
+            pdf.cell(90,20,txt ="Sex :",ln=0,align="R")
+            pdf.cell(100,20,txt = row.sex,ln=1,align="L")
+            pdf.cell(90,20,txt ="Phone :",ln=0,align="R")
+            pdf.cell(100,20,txt = row.phone,ln=1,align="L")
+            pdf.cell(90,20,txt ="State :",ln=0,align="R")
+            pdf.cell(100,20,txt = row.state,ln=1,align="L")
+            pdf.cell(90,20,txt ="City :",ln=0,align="R")
+            pdf.cell(100,20,txt = row.city,ln=1,align="L")
+            pdf.set_font("Arial",size=10)
+            pdf.cell(50,20,txt ="Symptoms :",ln=0,align="L")
+            pdf.cell(100,20,txt = row.symptoms,ln=1,align="L")            
+            pdf.output('report.pdf','F')
+
         return render_template('report.html',result=result)
 
-
     return render_template('report.html')
+
+
+
+if __name__ == "__main__":
+    app.run()
